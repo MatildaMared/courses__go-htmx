3a9d3df6899c89a2383e5f22b32129524b9c1e31
Matilda Mared <matildamared@live.se>
2024-04-13 08:24:34 +0200

    add working form

--- cmd/main.go
+++ cmd/main.go
@@ -22,6 +22,33 @@ func newTemplate() *Templates {
 	}
 }
 
+type Contact struct {
+	Name string
+	Email string
+}
+
+func newContact(name string, email string) Contact {
+	return Contact{
+		Name: name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func newData() Data {
+	return Data{
+		Contacts: Contacts{
+			newContact("John Doe", "john@gmail.com"),
+			newContact("Jane Doe", "jane@gmail.com"),
+		},
+	}
+}
+
 type Count struct {
 	Count int
 }
@@ -31,17 +58,20 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	count := Count { Count: 0 }
+	data := newData()
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", data)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		data.Contacts = append(data.Contacts, newContact(name, email))
+
+		return c.Render(200, "display", data)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
