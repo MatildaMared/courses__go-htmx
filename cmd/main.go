867f6543502aad6e74e2ae0b62ed033827312079
Matilda Mared <matildamared@live.se>
2024-04-13 10:04:26 +0200

    add loading indicator

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"strconv"
 	"text/template"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -104,6 +105,9 @@ func main() {
 
 	page := newPage()
 	e.Renderer = newTemplate()
+	
+	e.Static("/images", "images")
+	e.Static("/css", "css")
 
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(200, "index", page)
@@ -130,6 +134,8 @@ func main() {
 	})
 
     e.DELETE("/contacts/:id", func(c echo.Context) error {
+			time.Sleep(3 * time.Second)
+
         idStr := c.Param("id")
         id, err := strconv.Atoi(idStr)
         if err != nil {
